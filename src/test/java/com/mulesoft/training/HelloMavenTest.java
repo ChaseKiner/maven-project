2537441a51eef91fa35178f4aa86547e8b1a9460
ckiner <ckiner@acumensolutions.com>
2019-01-16 09:46:42 -0500

    Fix login

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,7 +1,8 @@
 package com.mulesoft.training;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
@@ -23,6 +24,11 @@ public class HelloMavenTest extends FunctionalTestCase {
       MuleEvent event = runFlow("retrieveFlights");
       String contentType = event.getMessage().getOutboundProperty("Content-Type");
       assertEquals("application/json", contentType);
+    }
+    
+    @BeforeClass
+    public static void setupPort() {
+    	System.setProperty("http.port", "18083");
     }
 
 }
